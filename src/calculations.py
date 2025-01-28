6ba06c840cb4e8a27c8275bd150ddd263991c7b9
ian hecker <ianhecker.dev@gmail.com>
2025-01-28 12:53:11 -0700

    Feature[Future Value Formula] Add formula

--- src/calculations.py
+++ src/calculations.py
@@ -1,3 +1,13 @@
 
-def calculate_future_value():
-	return 0
+def calculate_future_value(principal: int, gross_annual_return: float, compounds_per_year: int, years: int):
+	if compounds_per_year == 0:
+		return 0
+
+	compounding_periods = compounds_per_year * years
+
+	return principal * pow( 1 + ( gross_annual_return / compounds_per_year ), compounding_periods)
+
+
+# Principal * pow( 1 + (Gross Annual Return/Compounding Per Year), Compounding Periods)
+# Principal * pow( 1 + (Net Annual Return/Compounding Per Year), Compounding Periods)
+
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,4 +1,15 @@
+import pytest
 from src.calculations import calculate_future_value
 
-def test_calculate_future_value():
-	assert calculate_future_value() == 0
+@pytest.mark.parametrize(
+    "principal, gross_annual_return, compounds_per_year, years, expected",
+    [
+        (0, 0, 0, 0, 0),
+        (100, 0.01, 1, 1, 101.0),
+        (10000, 0.082, 4, 5,  15005.84),
+        (100000, 1.0, 4, 30, 4.25795984000815e+16),
+    ],
+)
+def test_calculate_future_value(principal, gross_annual_return, compounds_per_year, years, expected):
+    result = calculate_future_value(principal, gross_annual_return, compounds_per_year, years)
+    assert round(result, 2) == expected
